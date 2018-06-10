6a833d10508a9d1b85fc0d2779d26beddc9d0b14
akoziulia <akoziulia@cogniance.com>
2018-06-10 18:27:08 +0300

    React router

--- src/components/SavedMp3/SavedMp3.js
+++ src/components/SavedMp3/SavedMp3.js
@@ -1,11 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 
 class SavedMp3 extends Component {
   render () {
     console.log(this)
 
     return (
-      <h1>MP3</h1>
+      <Fragment>
+        <h1>MP3</h1>
+        {mp3Id && <h2>{this.props.match.params.mp3Id}</h2>}
+      </Fragment>
     )
   }
 }
